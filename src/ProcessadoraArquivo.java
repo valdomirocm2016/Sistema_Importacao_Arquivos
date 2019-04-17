49ba176ca32bc8bd1090eb4a4ef1cf0984ebf4c7
Valdomiro C.Martins <valdomirocm@gmail.com>
2019-04-17 09:41:46 -0300

    Update ProcessadoraArquivo.java
    Remoção de linhas de código comentadas

--- src/ProcessadoraArquivo.java
+++ src/ProcessadoraArquivo.java
@@ -19,20 +19,14 @@ public class ProcessadoraArquivo<E extends Lote> {
       
 	public List<E> processaArquivo(List<String> listaString)
 			throws IOException,ParseException,FileNotFoundException{
-		//LeituraArquivo leitura = new LeituraArquivo();
+		
 		List<E> listaConta = new ArrayList<>();
-                
-		//List<String> listaString = leitura.lerArquivo(arquivo);
-		//TODO implementar
 		String cabecalho = listaString.get(0);
-                
-                //DAOGenerico dt = new DAOGenerico();
-                System.out.println("Cabecalho "+cabecalho.toString());
+
 		listaString.remove(0);
+		
 		for (String linha: listaString) {
 			listaConta.add(processa.getLinha(linha,cabecalho));
-                        
-                        //dt.salvarAtualizar(processa.getLinha(linha));
                     
 		}
 		return listaConta; 		
